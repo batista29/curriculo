1850178cf91d53dd9ee963eee25798babccc4121
batista29 <natabatista2908@gmail.com>
2024-05-28 17:33:57 -0300

    Valores sendo pegos pelo javascript

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
-function carregar() {
-    var pag1 = document.getElementsByClassName("titulo").value
-    console.log(pag1)
-    pag1.classlist.remove("pag")
+function carregar(){
+    var pag1 = document.querySelector('.pag-1')
+    var pag3 = document.querySelector('.pag-3')
+    var pag4 = document.querySelector('.pag-4')
+    var pag5 = document.querySelector('.pag-5')
+    var pag6 = document.querySelector('.pag-6')
+    var pag7 = document.querySelector('.pag-7')
+    var pag8 = document.querySelector('.pag-8')
+
+    // pag1.classList.add('pag')
+    pag3.classList.add('pag')
+    pag4.classList.add('pag')
+    pag5.classList.add('pag')
+    pag6.classList.add('pag')
+    pag7.classList.add('pag')
+    pag8.classList.add('pag')
 }
